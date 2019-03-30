48f15ff19f839f929cfdf1e4177bc82c0fae3204
Wen Sun <syl1887415157@126.com>
2019-03-30 15:34:24 +0800

    fix: 修复学生反馈无法设置的bug

--- frontend/infoVerificationSys/src/views/admin/checkFeedback/CheckFeedback.vue
+++ frontend/infoVerificationSys/src/views/admin/checkFeedback/CheckFeedback.vue
@@ -15,22 +15,20 @@
 
             </el-table-column>
             <el-table-column header-align="center" align="center" fixed label="处理状态"
-                             width="100px" prop="isHandle" sortable >
+                             width="100px" prop="isHandle" sortable>
                 <template slot-scope="scope">
                     <el-button v-if="scope.row.isHandle" type="primary" plain disabled>已处理</el-button>
                     <el-button v-if="!scope.row.isHandle" type="danger" plain disabled>未处理</el-button>
                 </template>
 
             </el-table-column>
-            <el-table-column align="right" >
+            <el-table-column align="right">
                 <template slot-scope="scope">
                     <el-button size="medium" round icon="el-icon-star-off" type="danger"
-                               @click="changeFeedback(scope.row)"
-                               :disabled="scope.row.isHandle===true">标记为已处理
+                               @click="changeFeedback(scope.row)" :disabled="scope.row.isHandle===true">标记为已处理
                     </el-button>
                 </template>
             </el-table-column>
-
 
         </el-table>
     </el-container>
--- frontend/infoVerificationSys/src/views/admin/checkFeedback/checkFeedback.js
+++ frontend/infoVerificationSys/src/views/admin/checkFeedback/checkFeedback.js
@@ -1,82 +1,74 @@
 import {getFeedback, changeIsHandle} from '../../../utils/adminAPI'
 
 export default {
-  name: 'checkFeedback',
-  data () {
-    return {
-      feedback: [
-        {
-          stuIndex: 1,//编号
-          stuName: '学生A',//姓名
-          contact: '手机：123456789',//联系方式
-          remark: '中午没空',//备注
-          isHandle: false,//处理状态
-
-        },
-        {
-          stuIndex: 2,//编号
-          stuName: '学生E',//姓名
-          contact: '手机：123456789',//联系方式
-          remark: '中午没空',//备注
-          isHandle: true,//处理状态
-
-        },
-        {
-          stuIndex: 3,//编号
-          stuName: '学生C',//姓名
-          contact: '手机：123456789',//联系方式
-          remark: '中午没空',//备注
-          isHandle: false,//处理状态
-
-        },
-        {
-          stuIndex: 4,//编号
-          stuName: '学生D',//姓名
-          contact: '手机：123456789',//联系方式
-          remark: '中午没空',//备注
-          isHandle: true,//处理状态
-
-        },
-
-      ]
-
-    }
-  },
-  /*
-mounted{
-  this.$nextTick(() => {
-    this.getFeedback()
-  })
-}
-*/
-  method: {
-    changeFeedback (row) {
-      const stuId = row.stuIndex
-      for (let i=0;i<this.feedback.length;i++){
-        if(this.feedback[i].stuIndex===stuId){
-          this.feedback[i].isHandle.$set(true)
+    name: 'checkFeedback',
+    data() {
+        return {
+            feedback: [
+                {
+                    stuIndex: 1,//编号
+                    stuName: '学生A',//姓名
+                    contact: '手机：123456789',//联系方式
+                    remark: '中午没空',//备注
+                    isHandle: false,//处理状态
+                },
+                {
+                    stuIndex: 2,//编号
+                    stuName: '学生E',//姓名
+                    contact: '手机：123456789',//联系方式
+                    remark: '中午没空',//备注
+                    isHandle: true,//处理状态
+                },
+                {
+                    stuIndex: 3,//编号
+                    stuName: '学生C',//姓名
+                    contact: '手机：123456789',//联系方式
+                    remark: '中午没空',//备注
+                    isHandle: false,//处理状态
+                },
+                {
+                    stuIndex: 4,//编号
+                    stuName: '学生D',//姓名
+                    contact: '手机：123456789',//联系方式
+                    remark: '中午没空',//备注
+                    isHandle: true,//处理状态
+                },
+            ]
         }
-      }
-      changeIsHandle({
-          stuIndex: row.stuIndex,
-          isHandle: row.isHandle
-        }
-      )
-        .then(res => {
-          if (res.data.succeed) {
-            this.$message({
-              message: '编辑成功',
-              type: 'success'
-            })
-          } else {
-            this.$message({
-              message: res.data.msg,
-              type: 'error'
-            })
-          }
-        })
-
-    }
-
+    },
+    /*
+  mounted{
+    this.$nextTick(() => {
+      this.getFeedback()
+    })
   }
+  */
+    methods: {
+        changeFeedback(row) {
+            const stuId = row.stuIndex;
+            for (let i = 0; i < this.feedback.length; i++) {
+                if (this.feedback[i].stuIndex === stuId) {
+                    this.feedback[i].isHandle = true;
+                    break;
+                }
+            }
+            changeIsHandle({
+                    stuIndex: row.stuIndex,
+                    isHandle: row.isHandle
+                }
+            ).then(res => {
+                if (res.data.succeed) {
+                    this.$message({
+                        message: '编辑成功',
+                        type: 'success'
+                    })
+                } else {
+                    this.$message({
+                        message: res.data.msg,
+                        type: 'error'
+                    })
+                }
+            })
+        }
+    }
 }
