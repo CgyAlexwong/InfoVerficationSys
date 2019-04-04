4396a8561e8a8ba2ff9546410abf9432c69626d7
Weijin1455236662 <1455236662@qq.com>
2019-04-04 23:25:03 +0800

    修改开发模式访问的后端url地址
    seventeenth4_4

--- frontend/infoVerificationSys/src/utils/stuAPI.js
+++ frontend/infoVerificationSys/src/utils/stuAPI.js
@@ -4,7 +4,7 @@ import Cookies from 'js-cookie'
 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
 axios.defaults.baseURL = process.env.NODE_ENV === 'production'
-  ? 'http://101.132.35.81:3142'
+  ? 'http://120.78.76.9:3142'
   : 'http://localhost:3142'
 
 /*
