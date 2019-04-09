dac99f61362395ceb841db91de2fbcd13f5997a0
Weijin1455236662 <1455236662@qq.com>
2019-04-09 13:22:28 +0800

    添加人脸识别1
    eighteenth4_9

--- frontend/infoVerificationSys/src/views/student/faceVerify/faceVerify.vue
+++ frontend/infoVerificationSys/src/views/student/faceVerify/faceVerify.vue
@@ -9,16 +9,18 @@
     </mt-header>
     <p id="word">请拍摄你的正脸照：</p>
     <div id="camera">
-      <mt-button @click="appear">点击拍照</mt-button>
-      <input type="file" accept="image/jpeg,image/jpg,image/png" capture="camera">
+      <div class="a-upload">
+        <input id='cam' type="file" accept="image/*" capture="camera" @change="preview">
+        点击拍照
+      </div>
     </div>
-    <mt-actionsheet :actions="actions" v-model="sheetVisible"></mt-actionsheet>
+    <!--<mt-actionsheet :actions="actions" v-model="sheetVisible"></mt-actionsheet>-->
     <p id="word">预览：</p>
     <div id="load">
-      <img :src="img" alt="人脸照片" style="width: 80%">
+      <img :src="img" alt="人脸照片" style="width: 80%" @click="rotate">
     </div>
     <div id="commitBox">
-      <button id="commit" @click="go">提交</button>
+      <button id="commit" @click="submit">提交</button>
     </div>
   </div>
 </template>
@@ -27,7 +29,9 @@
 import MtButton from "mint-ui/packages/button/src/button";
 import MtHeader from "mint-ui/packages/header/src/header";
 import {MessageBox} from 'mint-ui'
-    export default {
+import {recognize} from "../../../utils/stuAPI";
+
+export default {
       name: "faceVerify",
       components: {MtHeader, MtButton},
       data(){
@@ -37,7 +41,8 @@ import {MessageBox} from 'mint-ui'
             method : this.getCamera	// 调用methods中的函数
           }],
           sheetVisible:false,
-          img:'https://github.com/CgyAlexwong/InfoVerficationSys/blob/uc1/frontend/infoVerificationSys/src/assets/da8e974dc.jpg?raw=true'
+          img:'https://github.com/CgyAlexwong/InfoVerficationSys/blob/uc1/frontend/infoVerificationSys/src/assets/da8e974dc.jpg?raw=true',
+          r : 0
         }
       },
       methods:{
@@ -54,7 +59,71 @@ import {MessageBox} from 'mint-ui'
             message:"你已完成前阶段任务，无法返回"
           })
         },
-
+        getCamera(){
+          const  video  =  document.getElementById('video');
+          const videoConstraints = {
+            facingMode: 'environment'
+          };
+          const constraints = {
+            video: videoConstraints,
+            audio: false
+          };
+          navigator.mediaDevices.getUserMedia(constraints)
+            .then(stream => {
+              video.srcObject = stream;
+            })
+            .catch(error => {
+              console.error(error);
+            });
+        },
+        /* show(){
+          let a = document.getElementById('cam').files[0];
+          console.log('name:'+a.name)
+          console.log('size:'+a.size)
+          console.log('type:'+a.type)
+          console.log('file:'+a)
+        }, */
+        preview(){
+          let file = document.getElementById('cam').files[0];
+          let reader = new FileReader();
+          reader.readAsDataURL(file);
+          console.log(reader)
+          let that = this
+          reader.onloadend = function () {
+            that.img = this.result;
+          }
+        },
+        rotate(){
+          let pic = document.getElementById('load');
+          this.r += 90;
+          pic.style.transform = 'rotate(' + this.r + 'deg)'
+        },
+        submit(){
+          let photo = document.getElementById('cam').files[0];
+          recognize({
+            photo:photo
+          }).then(res => {
+            if(res.succeed === true){
+              this.$router.push('/stu/OCRVerify')
+            }
+            else{
+              MessageBox.confirm('', {
+                message: res.msg,
+                title: '提示',
+                confirmButtonText: '反馈',
+                cancelButtonText: '重试'
+              }).then(action => {
+                if (action === 'confirm') {     //反馈的回调
+                  this.$router.push('/feedback')
+                }
+              }).catch(err => {
+                if (err === 'cancel') {     //重试的回调
+                  console.log("重试");
+                }
+              })
+            }
+          })
+        }
       }
     }
 </script>
@@ -67,6 +136,28 @@ import {MessageBox} from 'mint-ui'
     display: block;
     color: #888;
     text-align: left;
+  }
+  .a-upload {
+    position: relative;
+    display: inline-block;
+    background: #7645c1;
+    box-shadow: 0 0 1px #b8bbbf;
+    border-radius: 4px;
+    padding: 4px 12px;
+    overflow: hidden;
+    color: #ffffff;
+    text-decoration: none;
+    text-indent: 0;
+    line-height: 36px;
+  }
+  .a-upload input {
+    position: absolute;
+    font-size: 100px;
+    right: 0;
+    top: 0;
+    opacity: 0;
+    filter: alpha(opacity=0);
+    cursor: pointer
   }
   #camera{
     margin: 10px;
